3330018752583e69f60bdb9d6e4eaf664b49f7e6
timera-h <57534262+timera-h@users.noreply.github.com>
2020-03-14 22:53:21 +0100

    exercice css sission 1 et 2

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+//Hello world in 
+console.log("welcome @ Javascript:)");
+// on peut declarer simplement un object literal avec{}
+var cat = {
+    name: "fugee",
+    age: 0.6,
+    isCute: true,
+    isPain: true
+};
+// on peut utiliser le constructeur new object
+var catMoche = new Object({
+    name: "MalAimé",
+    age: 25,
+    isCute: false,
+    isPain: true
+});
+console.log(cat, catUgly);
+var simplonien = {
+    name: "kader",
+    lastname: "Amrichat",
+    age: 40,
+    frameworkJS: "Angular"
+};
+var simplonienSympaFirstname = "kader";
+console.log(simplonienSympaFirstname);
+simplonienSympaFirstname = "kader A";
+console.log(simplonienSympaFirstname);
+const monMotDePaseQuiNeChangeJamais = "1234";
+const myNumber = 10;
+// myNumber = 20 impossible de résigner une nouvel valeur à constante 
+const musicInstrument = {
+    type: "Synthétiseur"
+};
+console.log(musicIstrument);
+musicInstrument.price = 120; //la notation "." est appelée l'eccesseur...
+musicInstrument.currency = "euro";
+console.log
+// camelCase,
+// PascalCase,
+// kebab-Case
